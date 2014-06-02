80e1d5643618d8240ab2f9a534bfaa8edd087f8f
Matt Isner <matthew.isner@deque.com>
2014-06-01 22:17:18 -0400

    wip - new version

--- index.js
+++ index.js
@@ -7,87 +7,39 @@ var classes = require('classes');
 var query = require('querystring');
 
 /**
- * Expose `pagan`.
+ * Expose `Pagan`.
  */
 
-exports = module.exports = pagan;
+exports = module.exports = Pagan;
 
+var defaults = {
+  adjacent: 2
+};
 
-function pagan(elem, total, adj, path) {
-  total = total || 1;
-  adj = adj || 2;
-  path = path || location.pathname;
-
-  var curr = query.parse(location.search).p;
-  curr = curr ? parseInt(curr) : 1;
-
-  classes(elem).add('pagan');
-
-  if (curr > 1) {
-    elem.appendChild(pageLink(curr - 1, path, 'Prev'));
+function Pagan(el, options) {
+  if (!(this instanceof Pagan)) {
+    return new Pagan(el);
   }
+  this.wrapper = el.nodeType === 1
+    ? el
+    : typeof el === 'string'
+      ? document.querySelector(el)
+      : null;
+  this.options = options || {};
+  this.applyOptions();
+  console.log('this: ', this);
+}
 
-  var i = 1;
-  while (i <= total) {
-    if (i == curr) {
-      elem.appendChild(makeSpan('curr', i));
-    } else if (
-      (i >= (curr - adj) && i < curr) ||
-      (i <= (curr + adj) && i > curr) ||
-      (i === 1) ||
-      (i === total)
-    ) {
-      elem.appendChild(pageLink(i, path));
-    } else {
-      if (i < curr && !document.querySelector('.pagan .ell-left')) {
-        elem.appendChild(makeSpan('ell-left', '...'));
-      } else if (i > curr && !document.querySelector('.pagan .ell-right')) {
-        elem.appendChild(makeSpan('ell-right', '...'));
-      }
+Pagan.prototype.applyOptions = function () {
+  var that = this;
+  ['total', 'adjacent', 'path']
+  .forEach(function (opt) {
+    if (that.options.hasOwnProperty(opt)) {
+      that[opt] = that.options[opt];
+    } else if (defaults.hasOwnProperty(opt)) {
+      that[opt] = defaults[opt];
     }
+  });
+};
 
-    i++;
-  }
 
-  if (curr < total) {
-    elem.appendChild(pageLink(curr + 1, path, 'Next'));
-  }
-}
-
-/**
- * Generates a page link.
- *
- * @param {Number} toPage
- * @param {String} text
- * @param {String} path
- * @return {Element}
- * @api private
- */
-
-function pageLink(toPage, path, text) {
-  path = path || location.pathname;
-  text = text || toPage.toString();
-  var link = document.createElement('a');
-  link.href = path + '?p=' + toPage;
-  link.innerHTML = text;
-
-  var clipper = makeSpan('clip', 'Page');
-  link.insertBefore(clipper, link.firstChild);
-
-  return link;
-}
-
-/**
- * Generates a dummy link for the current page.
- *
- * @param {Number} page
- * @return {Element}
- * @api private
- */
-
-function makeSpan(classname, content) {
-  var elem = document.createElement('span');
-  classes(elem).add(classname);
-  elem.innerHTML = content;
-  return elem;
-}
